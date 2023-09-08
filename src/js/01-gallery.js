711b656bd3756a48b9aae13dd1ffe28109013cb0
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-09-09 07:31:10 +1200

    add simplelightbox gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const container = document.querySelector('.gallery');
+
+const markup = createMarkup(galleryItems);
+
+container.insertAdjacentHTML('beforeend', markup);
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery__link', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
 
 console.log(galleryItems);
