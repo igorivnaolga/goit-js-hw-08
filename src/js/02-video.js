f976d17111a3012217b23150abfdfd5f199a92ac
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-09-09 19:48:49 +1200

    update video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', function () {
+  let time = player.getCurrentTime;
+  console.log(time);
+});
